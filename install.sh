db0246e797c9833c60986b663e418f0fea1a9d26
Marvin Ewald <marvin.e@protonmail.com>
2019-04-19 18:35:16 +0200

    Restructure - Fix and improve install script

--- install.sh
+++ install.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 
-cd $(dirname $(readlink -f "$0"))
-shopt -s extglob
-
-homedir=~
-dotdir="$(dirname "$(readlink -f "$0")")" 
-
-# A (blacklist) pattern to match all files that will be automatically linked to $homedir.
-files=(!(.|..|.git|.gitignore|.gitmodules|README|LICENSE|install.sh|.config|firefox))
-
-install() {
-    ln -svT "$PWD/$1" "$homedir/$i"
-}
-uninstall() {
-    rm -v "$homedir/$1"
-}
-
+dotdir="$(dirname "$(readlink -f "$0")")"
 cd "$dotdir"
 
-f=install
-if [ "$1" = "--uninstall" -o "$1" = "-u" ]; then
-    f=uninstall
-fi
+shopt -s extglob
+GLOBIGNORE=".:.."
 
-for i in "${files[@]}"; do
-    $f "$i"
-done
+userhome=~
 
-mkdir -pv "$homedir/.config"
-for i in .config/*; do
-    $f "$i"
-done
 
-mkdir -p "$homedir/.ipython/profile_default"
-$f .ipython/profile_default/ipython_config.py
+# arg1: relative source dir
+# arg2: dest dir
+install_dir() {
+    mkdir -p "$2"
+    cd "$2"
+    ln -svi -t . "$dotdir/$1/"*
+    cd -
+}
+
+install_dir homedir "$userhome"
+install_dir configdir "$userhome/.config"
+install_dir ipython/profile_default "$userhome/.ipython/profile_default"
